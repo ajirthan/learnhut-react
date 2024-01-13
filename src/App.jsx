8356139a8a5bfbebf8b9cd3e6477a6c2d6b87f11
Ajirthan <ajirbala28@gmail.com>
2024-01-13 21:15:15 +0530

    feat: add more personal information

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,31 @@ const App = () => {
         <h1>
           My Name is Ajirthan Balasingham.
         </h1>
+        <h3>About Me</h3>
+        <p>
+          I am a Full Stack Developer.<br/> I have experience in React, Node.js,
+          Express.js, MongoDB, MySQL, HTML, CSS, JavaScript, Python, Java, C,
+          C++, C#, and PHP.<br/> I am a quick learner and I am always looking to
+          learn new technologies.<br/> I am a team player and I am always
+          willing to
+          help others.
+        </p>
+        <h3>Education</h3>
+        <p>
+          I have learnt Full Stack Web Development at Uki Coding School, Jaffna
+          in November 2022.<br/> I am pursuing a Bachelors in IT at the
+          University of
+          Colombo School of Computing.<br/> Also, I am a learner at the Calcey
+          Springboard Program, a 12 month scholarship program to<br/> learn
+          skills needed to become a successful Software Engineer.<br/>
+        </p>
+        <h3>Contact</h3>
+        <p>
+          ✉️ email: ajirbala28@gmail.com<br/>
+          📱 phone: +9476 3695 751<br/>
+          📪 place: Mallavi, Mullaitivu, Sri Lanka<br/>
+          🌐 website: ajirthan.github.io<br/>
+        </p>
       </div>
   );
 };
